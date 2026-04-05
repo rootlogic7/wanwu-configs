41354980223c17ab63a2e84084438727b1b03697
haku <rootlogic7@proton.me>
2026-04-05 17:50:35 +0200

    Update user.nix

--- modules/common/user.nix
+++ modules/common/user.nix
@@ -1,8 +1,16 @@
-{ config, pkgs, ... }: {
-  users.users.zhenren = {
-    isNormalUser = true;
-    description = "Zhenren";
-    extraGroups = [ "networkmanager" "wheel" "video" ];
-    hashedPasswordFile = config.sops.secrets."users/zhenren/password".path;
+{ config, pkgs, lib, ... }: {
+  # 1. Option definieren
+  options.mainUser = lib.mkOption {
+    type = lib.types.str;
+    default = "haku";
+    description = "Primary System User";
+  };
+  config = {
+    users.users.${config.mainUser} = {
+      isNormalUser = true;
+      description = "Primary System User";
+      extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
+      hashedPasswordFile = config.sops.secrets."users/${config.mainUser}/password".path;
+    };
   };
 }
