d1d0336b69d40ec615463feb6cba39fb2fe3e83b
haku <rootlogic7@proton.me>
2026-04-05 18:13:09 +0200

    Update default.nix

--- hosts/qian/default.nix
+++ hosts/qian/default.nix
@@ -1,14 +1,19 @@
 { ... }: {
   imports = [
-    ../../modules/common
-    ../../modules/profiles/workstation.nix
-    ../../modules/hardware/nvidia.nix
-    ../../modules/desktops/niri.nix
     ./disko.nix
     # ./hardware-configuration.nix
+    ../../modules/common
+    ../../modules/profiles/workstation.nix
+    ../../modules/hardware/zfs-rollback.nix
+    ../../modules/hardware/nvidia.nix
+    ../../modules/desktops/niri.nix
   ];
   mainUser = "haku";
   custom.hardware.nvidia.enable = true;
+  custom.hardware.zfs-rollback = {
+    enable = true;
+    pool = "rpool";
+  };
 
   networking.hostName = "workstation";
   networking.hostId = "1234abcd";
